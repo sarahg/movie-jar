cc8ffc2860c4c504e4a0ec84dc014b55847d62ff
Sarah German <sgerman@pantheon.io>
2020-08-19 14:40:39 -0500

    Pick a random list item.

--- app.js
+++ app.js
@@ -14,12 +14,16 @@ app.get("/", (req, res) => {
   any.login().then(async () => {
     await any.getLists();
 
+    let movies = [];
+
     // Get the Movie Jar list.
     const movieList = any.getListByName(process.env.ANYLIST_LIST_NAME);
-    console.log(movieList);
+    for (var i = 0, len = movieList.items.length; i < len; i++) {
+      movies.push(movieList.items[i]._name);
+    }
 
     // Return a random item from the list.
-    let movie = "";
+    let movie = movies[Math.floor(Math.random() * movies.length)];
 
     any.teardown();
 
