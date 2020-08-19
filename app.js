0ef6bb498b1c4caaf6d092b6a91cc0c07e8afc0d
Sarah German <sgerman@pantheon.io>
2020-08-19 13:56:21 -0500

    Scaffold express app with an anylist API call.

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  const AnyList = require("anylist");
+
+  const any = new AnyList({
+    email: "",
+    password: "",
+  });
+
+  any.login().then(async () => {
+    await any.getLists();
+
+    // Get the Movie Jar list.
+    const movieList = any.getListByName("Movie Jar");
+    console.log(movieList);
+
+    any.teardown();
+  });
+
+  res.send("Hello World!!!!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
