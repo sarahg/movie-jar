a984e2761ada28f8aec69d7c8a89e9cd8d329e95
Sarah German <sgerman@pantheon.io>
2020-08-19 14:30:43 -0500

    Authenticate with AnyList using env vars

--- app.js
+++ app.js
@@ -1,26 +1,30 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+require("dotenv").config();
 
 app.get("/", (req, res) => {
   const AnyList = require("anylist");
 
   const any = new AnyList({
-    email: "",
-    password: "",
+    email: process.env.ANYLIST_EMAIL,
+    password: process.env.ANYLIST_PASSWORD,
   });
 
   any.login().then(async () => {
     await any.getLists();
 
     // Get the Movie Jar list.
-    const movieList = any.getListByName("Movie Jar");
+    const movieList = any.getListByName(process.env.ANYLIST_LIST_NAME);
     console.log(movieList);
 
-    any.teardown();
-  });
+    // Return a random item from the list.
+    let movie = "";
 
-  res.send("Hello World reloaded!!!");
+    any.teardown();
+
+    res.send(`Random movie: ${movie}`);
+  });
 });
 
 app.listen(port, () => {
